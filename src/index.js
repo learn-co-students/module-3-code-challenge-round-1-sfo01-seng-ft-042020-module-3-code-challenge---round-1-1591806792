b73231be8cb53a6698755563279960c82bc8a255
stellarwobble <jennidemuir@gmail.com>
2020-06-10 11:18:23 -0700

    doneish

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 5419
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -10,3 +10,46 @@ document.addEventListener('DOMContentLoaded', () => {
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
 })
+
+fetch('https://randopic.herokuapp.com/images/5419')
+.then(resObj => resObj.json())
+.then(data => { console.log(data)
+  const img = document.getElementById('image');
+  const h4 = document.getElementById('name')
+  const likes = document.getElementById('likes')
+  const comments = document.getElementById('comments')
+  
+ 
+  const newComment = (data.comments[0].content)
+  
+ 
+  img.src = data.url;
+  h4.innerHTML = data.name;
+  likes.innerHTML = data.like_count
+  comments.innerHTML += `<p>${newComment}</p>`
+
+  const likeButton = (document.getElementById('like_button'))
+
+  likeButton.addEventListener('click', (e) => {
+  if(e.target.tagName === 'BUTTON'){
+    
+    let num = parseInt(likes.innerText)
+    num += 1
+    likes.innerText = num
+
+    fetch("https://randopic.herokuapp.com/likes", {
+      method: "PATCH",
+      header: {
+      "Accept": 'application/json',
+      "Content-Type": 'application/json'
+      },
+      body: JSON.stringify ({
+        "image_id": 5419,
+        "like_count": num
+      })
+    })
+  }
+  })
+})
+
+
